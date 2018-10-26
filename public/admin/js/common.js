0f523aeefbc37d140e20706e6db8325c417d336f
litao2 <913490976@qq.com>
2018-10-26 21:47:46 +0800

    首页完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -9,3 +9,39 @@ $(document).ajaxStop(function (){
   // 结束进度条
 NProgress.done()
 })
+
+//  二级分类 
+// 1.先注册点击事件
+$('.category>a').on('click', function(){
+  // 2.在通过下一个兄弟元素添加显示隐藏
+  $(this).next().slideToggle()
+})
+
+// 侧边栏显示隐藏
+$('.icon_menu').on('click',function() {
+  // 2.给元素添加隐藏功能
+  $('body').toggleClass('active')
+  $('.lt_aside').toggleClass('active')
+})
+
+// 1.给退出按钮注册点击事件
+// 2.弹出模态框
+// 3.等用户点击的时候在退出
+$('.icon_logout').on('click',function (){
+  // 弹出模态框
+  $('#logoutModal').modal('show')
+})
+
+$('.logout').on('click',function () {
+  // 发送ajax请求
+  $.ajax({
+    type:'get',
+    url:'/employee/employeeLogout',
+    success:function(info) {
+      if(info.success) {
+        // 退出成功，跳转到登录页面
+        location.href = 'login.html'
+      }
+    }
+  })
+})
---
+++ public/admin/js/index.js
@@ -0,0 +1,62 @@
+var myChart = echarts.init(document.querySelector('.chart_left'))
+var chartRight = echarts.init(document.querySelector('.chart_right'))
+ // 指定图表的配置项和数据
+ var option = {
+  title: {
+      text: '2018年注册人数'
+  },
+  tooltip: {},
+  legend: {
+      data:['人数']
+  },
+  xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+  },
+  yAxis: {},
+  series: [{
+      name: '人数',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+  }]
+};
+myChart.setOption(option);
+
+ var right = {
+  title : {
+      text: '热门品牌销售',
+      subtext: '2018年8',
+      x:'center'
+  },
+  tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+  },
+  legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['耐克','李宁','优衣库','阿迪','新百伦']
+  },
+  series : [
+      {
+          name: '访问来源',
+          type: 'pie',
+          radius : '55%',
+          center: ['50%', '60%'],
+          data:[
+              {value:335, name:'耐克'},
+              {value:310, name:'李宁'},
+              {value:234, name:'优衣库'},
+              {value:135, name:'阿迪'},
+              {value:1548, name:'新百伦'}
+          ],
+          itemStyle: {
+              emphasis: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+          }
+      }
+  ]
+}
+chartRight.setOption(right)
